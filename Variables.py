6a5b16fbfbe280750068ae28e6448e5675377a76
MARTINA LEON M <emleonm@unal.edu.co>
2022-05-30 21:44:10 -0500

    first commit

---
+++ Variables.py
@@ -0,0 +1,19 @@
+
+name = "Frozen"
+print (name)
+
+# case sensitive - Son variables diferentes con nombre semejante
+Book: "Rojo"
+book: "ROJO"
+
+#En conjunto - se crean x , x  variables
+x , Roro = 10  "mar"
+print (x , roro)
+
+#Convenciones para nomenclatura
+book_name = "el viaje al centro" # Snake case
+bookName = "el viaje al centro" # Camel cas
+BookName =  "el viaje al centro" #Pascal case
+
+#CUANDO EL NOMBRE VA EN MAYUSCULA, PARA DEFINIR INTERNAMENTE
+BOOK = 3.24 #Dejar por convension que la variable no cambie.
